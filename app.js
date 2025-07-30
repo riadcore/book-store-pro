54c39f66179a0fda7d38075e827af33702f8736d
Riadul Islam <riadcore@email.com>
2025-07-30 21:04:43 +0600

    Fix: update CORS or API base URL in app.js

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ dotenv.config();
 
 const app = express();
 
-app.use(cors({ origin: 'http://localhost:3000', credentials: true }));
+app.use(cors({ origin: 'https://book-store-pro-front-end.vercel.app/', credentials: true }));
 app.use(express.json());
 
 app.use('/auth', authRoutes);
